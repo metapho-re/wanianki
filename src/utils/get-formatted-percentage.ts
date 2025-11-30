9e6068c467f77a5f17e6930dc11bb8e4a33258c3
Pierre-Alain Castella <pierrealain.castella@gmail.com>
2025-11-30 21:31:33 +0100

    feat: Display N/A in quiz report when no item of given type were reviewed.

--- src/utils/get-formatted-percentage.ts
+++ src/utils/get-formatted-percentage.ts
@@ -3,7 +3,7 @@ export const getFormattedPercentage = (
   total: number,
 ): string => {
   if (total === 0) {
-    return "0%";
+    return "N/A";
   }
 
   return `${((correct / total) * 100).toFixed(0)}%`;
